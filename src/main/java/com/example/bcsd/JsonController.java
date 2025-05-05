03c41c7af405f08abf4bf694003a9e94edf155fc
NIF404 <dlgkdnjs2418@gmail.com>
2025-05-05 12:17:16 +0900

    refactor : /json 기능을 RestController를 이용해 리팩토링

--- src/main/java/com/example/bcsd/JsonController.java
+++ src/main/java/com/example/bcsd/JsonController.java
@@ -1,21 +1,18 @@
 package com.example.bcsd;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
 
-@Controller
+@RestController
 public class JsonController {
 
     @GetMapping("/json")
-    public String json(@RequestParam(name="age", required=false, defaultValue="23") int age,
-                       @RequestParam(name = "name", required = false, defaultValue = "이하원") String name,
-                       Model model) {
-        model.addAttribute("age", age);
-        model.addAttribute("name", name);
-        return "json";
+    public Map<String, Object> json() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("age", 26);
+        map.put("name", "허준기");
+        return map;
     }
-
 }
