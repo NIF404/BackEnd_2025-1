ecbd0e5a13fac840d7658d6bcf81811f6396d882
NIF404 <dlgkdnjs2418@gmail.com>
2025-05-05 10:00:47 +0900

    feat : /introduce?name 기능 추가

--- src/main/java/com/example/bcsd/IntroduceController.java
+++ src/main/java/com/example/bcsd/IntroduceController.java
@@ -1,14 +1,17 @@
 package com.example.bcsd;
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IntroduceController {
 
     @GetMapping("/introduce")
-    public String introduce() {
+    public String introduce(@RequestParam(name="name", required=false, defaultValue="이하원") String name, Model model) {
+        model.addAttribute("name", name);
         return "introduce";
     }
 
