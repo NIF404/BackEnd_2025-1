7fe8b9b78a58b44ac0b5c2512ca173b6eeff5257
NIF404 <dlgkdnjs2418@gmail.com>
2025-05-26 01:26:13 +0900

    feat : 예외처리를 위한 헨들러 클래스 구현

--- src/main/java/com/example/bcsd/DBExceptionHandler.java
+++ src/main/java/com/example/bcsd/DBExceptionHandler.java
@@ -2,15 +2,34 @@ package com.example.bcsd;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.ErrorResponse;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class DBExceptionHandler {
 
     @ExceptionHandler(EmailAlreadyExistsException.class)
-    public ResponseEntity<ErrorResponse> handleEmailAlreadyExistsException(EmailAlreadyExistsException e){
-        return ResponseEntity<>(e.)
+    public ResponseEntity<String> handleEmailAlreadyExistsException(EmailAlreadyExistsException e){
+        return ResponseEntity.status(e.getStatusCode()).body(e.getMessage());
     }
 
+    @ExceptionHandler(EntityHasArticleException.class)
+    public ResponseEntity<String> handleEntityHasArticleException(EntityHasArticleException e){
+        return ResponseEntity.status(e.getStatusCode()).body(e.getMessage());
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException e){
+        return ResponseEntity.status(e.getStatusCode()).body(e.getMessage());
+    }
+
+    @ExceptionHandler(InvalidRequestBodyException.class)
+    public ResponseEntity<String> handleInvalidRequestBodyException(InvalidRequestBodyException e){
+        return ResponseEntity.status(e.getStatusCode()).body(e.getMessage());
+    }
+
+    @ExceptionHandler(ReferencedEntityNotFoundException.class)
+    public ResponseEntity<String> handleReferencedEntityNotFoundException(ReferencedEntityNotFoundException e){
+        return ResponseEntity.status(e.getStatusCode()).body(e.getMessage());
+    }
 }
